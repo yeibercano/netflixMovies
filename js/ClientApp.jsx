a717c535f3859b5079aeb9bbdc7d4136f60a6809
Yeiber Cano <yeibercano@gmail.com>
2016-08-21 00:41:48 -0700

    change extension

---
+++ js/ClientApp.jsx
@@ -0,0 +1,14 @@
+const React = require('react')
+const ReactDOM = require('react-dom')
+const MyTitle = require('./MyTitle')
+
+const MyFirstComponent = function() {
+  return (
+    <div>
+     <MyTitle title="That's wasup" color='rebeccapurple' />
+     <MyTitle title="Why not" color='peru' />
+     <MyTitle title="Why yes" color='tomatos' />
+    </div>
+  )
+}
+ReactDOM.render(<MyFirstComponent />, document.getElementById('app'))
