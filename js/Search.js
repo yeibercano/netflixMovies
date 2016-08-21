132d2f37ebb151855cd6342a78675eefde21ac99
Yeiber Cano <yeibercano@gmail.com>
2016-08-21 12:14:59 -0700

    render data from Json

--- js/Search.js
+++ js/Search.js
@@ -4,7 +4,14 @@ const data = require('../public/data')
 const Search = () => (
   <div className="container">
     {data.shows.map((show) => (
-      <h3>{show.title}</h3>
+      <div className="show">
+        <img src={`public/img/posters/${show.poster}`} className="show-img" alt={show.title} />
+        <div className="show-text">
+          <h3 className="show-title">{show.title}</h3>
+          <h4 className="show-year">({show.year})</h4>
+          <p className="show-description">{show.description}</p>
+        </div>
+      </div>
     ))}
   </div>
 )
