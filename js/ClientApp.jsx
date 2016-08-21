b9619707ab76638ec5459739c2ece48cdd2543a6
Yeiber Cano <yeibercano@gmail.com>
2016-08-21 11:52:05 -0700

    create search component and link to it

--- js/ClientApp.jsx
+++ js/ClientApp.jsx
@@ -2,10 +2,11 @@ const React = require('react')
 const ReactDOM = require('react-dom')
 const Landing = require('./Landing')
 const { Router, Route, hasHistory } = require('react-router')
-
+const Search = require('./Search')
 const App = () => (
   <Router history={hasHistory}>
     <Route path='/' component={Landing} />
+    <Route path='/search' component={Search} />
   </Router>
 )
 
