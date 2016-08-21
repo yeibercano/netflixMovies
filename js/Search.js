870146b5e5c78d7255d752cb2bde1bf17188e450
Yeiber Cano <yeibercano@gmail.com>
2016-08-21 14:56:20 -0700

    create logic for searchTerm

--- js/Search.js
+++ js/Search.js
@@ -3,15 +3,32 @@ const data = require('../public/data')
 const ShowCard = require('./ShowCard')
 
 const Search = React.createClass({
+  getInitialState() {
+    return {
+      searchTerm: ''
+    }
+  },
+  handleSearchTermEvent(e) {
+    e.preventDefault()
+    this.setState({ searchTerm: e.target.value })
+  },
+
   render() {
     return (
       <div className="container">
         <header className="header">
           <h1 className="brand">svideo</h1>
-          <input type="text" placeholder='search' className="search-input"/>
+          <input 
+          value= {this.state.searchTerm}
+          onChange={this.handleSearchTermEvent}
+          type="text" 
+          placeholder='search' 
+          className="search-input"/>
         </header>
         <div className="shows">
-          {data.shows.map((show) => (
+          {data.shows
+            .filter((show) => `${show.title} ${show.description}`.toUpperCase().indexOf(this.state.searchTerm.toUpperCase()) >=0)
+            .map((show) => (
             <ShowCard {...show} key={show.imdbID} />
           ))}
         </div>
