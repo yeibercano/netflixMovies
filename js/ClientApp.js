5df9d1d45e07c8b65a7c6c6fdfd7d47bdedc7e72
Yeiber Cano <yeibercano@gmail.com>
2016-08-20 21:33:12 -0700

    different ways of creating elements

--- js/ClientApp.js
+++ js/ClientApp.js
@@ -2,9 +2,24 @@
 var div = React.DOM.div
 var h1 = React.DOM.h1
 
+var MyTitle = React.createClass({
+  render() {
+    return (
+      div(null,
+        h1(null, 'Another component')
+      )
+    )
+  }
+})
+
+var MyTitleFact = React.createFactory(MyTitle)
+var ce = React.createElement
+
 var MyFirstComponent = (
-	div(null,
-		h1(null, 'This is my first component')
+  div(null,
+  	MyTitleFact(null),
+    React.createElement(MyTitle, null),
+    ce(MyTitle, null)
 	)
 )
 ReactDOM.render(MyFirstComponent, document.getElementById('app'))
