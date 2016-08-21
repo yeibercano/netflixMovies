7ff84146351b24001c8cc148920a358e4b890635
Yeiber Cano <yeibercano@gmail.com>
2016-08-21 12:40:13 -0700

    import ShowCard component

--- js/Search.js
+++ js/Search.js
@@ -1,18 +1,14 @@
 const React = require('react')
 const data = require('../public/data')
+const ShowCard = require('./ShowCard')
 
 const Search = () => (
   <div className="container">
-    {data.shows.map((show) => (
-      <div className="show">
-        <img src={`public/img/posters/${show.poster}`} className="show-img" alt={show.title} />
-        <div className="show-text">
-          <h3 className="show-title">{show.title}</h3>
-          <h4 className="show-year">({show.year})</h4>
-          <p className="show-description">{show.description}</p>
-        </div>
-      </div>
-    ))}
+    <div className="shows">
+      {data.shows.map((show) => (
+       <ShowCard show={show} />
+      ))}
+    </div>
   </div>
 )
 
