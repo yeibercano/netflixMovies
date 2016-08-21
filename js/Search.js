ca715b03e41011dc8923f417ee9cf3f32e535d03
Yeiber Cano <yeibercano@gmail.com>
2016-08-21 14:14:18 -0700

    refactor to statefull component

--- js/Search.js
+++ js/Search.js
@@ -2,18 +2,22 @@ const React = require('react')
 const data = require('../public/data')
 const ShowCard = require('./ShowCard')
 
-const Search = () => (
-  <div className="container">
-    <header className="header">
-      <h1 className="brand">svideo</h1>
-      <input type="text" placeholder='search' className="search-input"/>
-    </header>
-    <div className="shows">
-      {data.shows.map((show) => (
-        <ShowCard {...show} key={show.imdbID} />
-      ))}
-    </div>
-  </div>
-)
+const Search = React.createClass({
+  render() {
+    return (
+      <div className="container">
+        <header className="header">
+          <h1 className="brand">svideo</h1>
+          <input type="text" placeholder='search' className="search-input"/>
+        </header>
+        <div className="shows">
+          {data.shows.map((show) => (
+            <ShowCard {...show} key={show.imdbID} />
+          ))}
+        </div>
+      </div>
+    )
+  }
+})
 
 module.exports = Search
