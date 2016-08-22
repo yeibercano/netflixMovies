9f6c3cf061397ae1d21a5e167411a142dcedaef3
Yeiber Cano <yeibercano@gmail.com>
2016-08-21 21:04:07 -0700

    setup test framework

---
+++ test/App.spec.js
@@ -0,0 +1,12 @@
+/* eslint-env mocha */ 
+
+const { expect } = require('chai')
+
+describe('<Search />', () => {
+  it('should pass', () => {
+    expect(1 + 1 === 2).to.be.true
+  })
+  it('should equal', () => {
+    expect(3+ 2).to.equal(5)
+  })
+})
