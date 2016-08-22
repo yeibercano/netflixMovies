35301a635593af2ebfa3219e09a22098a720c73f
Yeiber Cano <yeibercano@gmail.com>
2016-08-21 21:15:17 -0700

    test render branding

--- test/App.spec.js
+++ test/App.spec.js
@@ -1,12 +1,14 @@
 /* eslint-env mocha */ 
 
 const { expect } = require('chai')
+const React = require('react')
+const Search = require('../js/Search')
+const { shallow } = require('enzyme')
 
 describe('<Search />', () => {
-  it('should pass', () => {
-    expect(1 + 1 === 2).to.be.true
+  it('should render the brand', () => {
+    const wrapper = shallow(<Search />)
+    expect(wrapper.contains(<h1 className="brand">svideo</h1>)).to.be.true
   })
-  it('should equal', () => {
-    expect(3+ 2).to.equal(5)
-  })
+ 
 })
