4eb0f06f725c4207f654104e1c177ecab4994951
Yeiber Cano <yeibercano@gmail.com>
2016-08-21 22:50:50 -0700

    test the length of cards render

--- test/App.spec.js
+++ test/App.spec.js
@@ -3,12 +3,18 @@
 const { expect } = require('chai')
 const React = require('react')
 const Search = require('../js/Search')
+const ShowCard = require('../js/ShowCard')
 const { shallow } = require('enzyme')
+const { shows } = require('../public/data')
 
 describe('<Search />', () => {
   it('should render the brand', () => {
     const wrapper = shallow(<Search />)
     expect(wrapper.contains(<h1 className="brand">svideo</h1>)).to.be.true
+  })
+  it('render same length of cards as data', () => {
+    const wrapper = shallow(<Search />)
+    expect(wrapper.find(ShowCard).length).to.equal(shows.length)
   })
  
 })
