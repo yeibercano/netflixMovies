d418e674cc74955858b5bd21f92bd253c2073dd9
Yeiber Cano <yeibercano@gmail.com>
2016-08-22 18:02:20 -0700

    refactor to ES6 class

--- js/Landing.js
+++ js/Landing.js
@@ -1,12 +1,16 @@
 const React = require('react')
 const { Link } = require('react-router')
 
-const Landing = () => (
-  <div className='home-info'>
-    <h1 className="title">svideo</h1>
-    <input className="search" type="text" placeholder="Search"></input>
-    <Link to="/search" className="browse-all">or browse all</Link>
-  </div>
-)
+class Landing extends React.Component {
+  render () {
+    return (
+      <div className='home-info'>
+        <h1 className="title">svideo</h1>
+        <input className="search" type="text" placeholder="Search"></input>
+        <Link to="/search" className="browse-all">or browse all</Link>
+      </div>
+    )
+  }
+}
 
 module.exports = Landing
