da5c9c36434dc45b26d78f29bc451d90d6b4be31
Yeiber Cano <yeibercano@gmail.com>
2016-08-23 15:16:34 -0700

    refactor childRoutes

--- js/ClientApp.jsx
+++ js/ClientApp.jsx
@@ -1,53 +1,69 @@
 const React = require('react')
 const Layout = require('./Layout')
-const { Router, Route, browserHistory, IndexRoute } = require('react-router')
-const { store } = require('./Store')
-const { Provider } = require('react-redux')
+const ReactRouter = require('react-router')
+const { Router, browserHistory } = ReactRouter
+const Store = require('./Store')
+const { store } = Store
+const reactRedux = require('react-redux')
+const { Provider } = reactRedux
 
-if (typeof module !== undefined && module.require) {
+if (typeof module !== 'undefined' && module.require) {
   if (typeof require.ensure === 'undefined') {
-    require.ensure = require('node-ensure') // shim for node.js
+    require.ensure = require('node-ensure')// shim for node.js
   }
 }
 
 const rootRoute = {
-  component: Layout, 
-  path:'/',
-  IndexRoute: {
+  component: Layout,
+  path: '/',
+  indexRoute: {
     getComponent (location, cb) {
-      rquire.ensure([], (error) => {
+      require.ensure([], (error) => {
+        if (error) {
+          return console.error('ClientApp Landing require.ensure error', error)
+        }
         cb(null, require('./Landing'))
       })
     }
   },
   childRoutes: [
-    path: 'search',
-    getComponent (location, cb) {
-      require.ensure([], (error) => {
-        cb(null, require('./Search'))
-      })
+    {
+      path: 'search',
+      getComponent (location, cb) {
+        require.ensure([], (error) => {
+          if (error) {
+            return console.error('ClientApp Search require.ensure error', error)
+          }
+          cb(null, require('./Search'))
+        })
+      }
     },
-    path: 'details/:id',
-    getComponent (location, cb) {
-      require.ensure([], (error) => {
-        cb(null, require('./Details'))
-      })
+    {
+      path: 'details/:id',
+      getComponent (location, cb) {
+        require.ensure([], (error) => {
+          if (error) {
+            return console.error('ClientApp Details require.ensure error', error)
+          }
+          cb(null, require('./Details'))
+        })
+      }
     }
   ]
 }
 
-
-const App = React.createClass({
+class App extends React.Component {
   render () {
     return (
       <Provider store={store}>
-        <Router history={browserHistory} routes={rootRoute}/>
+        <Router routes={rootRoute} history={browserHistory} />
       </Provider>
     )
   }
-})
+}
 
+// App.Routes = myRoutes
 App.Routes = rootRoute
-App.history = browserHistory
+App.History = browserHistory
 
 module.exports = App
