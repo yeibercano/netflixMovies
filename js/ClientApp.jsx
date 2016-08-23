380ec6b89a688715f4e95cf6b7726fe300f1f3d9
Yeiber Cano <yeibercano@gmail.com>
2016-08-22 20:13:29 -0700

    modify to connect to server

--- js/ClientApp.jsx
+++ js/ClientApp.jsx
@@ -7,20 +7,26 @@ const Details = require('./Details')
 const { store } = require('./Store')
 const { Provider } = require('react-redux')
 
+const myRoutes = () => (
+  <Route path='/' component={Layout}>
+    <IndexRoute component={Landing} />
+    <Route path='/search' component={Search} />
+    <Route path='/details/:id' component={Details} />
+  </Route>
+)
+
 const App = React.createClass({
   render () {
     return (
       <Provider store={store}>
         <Router history={browserHistory}>
-          <Route path='/' component={Layout}>
-            <IndexRoute component={Landing} />
-            <Route path='/search' component={Search} />
-            <Route path='/details/:id' component={Details} />
-          </Route>
+          {myRoutes()}
         </Router>
       </Provider>
     )
   }
 })
+
+App.Routes = myRoutes
 
 module.exports = App
