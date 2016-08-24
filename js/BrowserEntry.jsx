24c37b0e8e141402724dd2a955aedb7351fee95d
Yeiber Cano <yeibercano@gmail.com>
2016-08-24 01:16:28 -0700

    pass history and route to app from server

--- js/BrowserEntry.jsx
+++ js/BrowserEntry.jsx
@@ -1,12 +1,12 @@
 const React = require('react')
 const ReactDOM = require('react-dom')
-const App = require('./ClientApp')
-const { match } require('react-router')
+const App = require('./ClientApp.jsx')
+const { match } = require('react-router')
 
 match({ history: App.history, routes: App.Routes }, (error, redirectLocation, renderProps) => {
   if (error) {
     return console.log.error('BrowserEntry error', error)
   }
-  ReactDOM.render(<App {...renderProps}/>, document.getElementById('app'))
+  ReactDOM.render(<App {...renderProps} />, document.getElementById('app'))
 })
 
